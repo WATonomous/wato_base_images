0e2663e7718be8ad58bede1eecc40c0e8b2cd907
Eddy Zhou <edward.zhzh@gmail.com>
2024-10-16 16:47:17 -0400

    pass in repository name for docker registry

--- .github/templates/docker_context/docker_context.sh
+++ .github/templates/docker_context/docker_context.sh
@@ -3,5 +3,7 @@
 REGISTRY_URL="ghcr.io/watonomous/robot_base"
 
 REGISTRY=$(echo "$REGISTRY_URL" | sed 's|^\(.*\)/.*$|\1|')
+REPOSITORY=$(echo "$REGISTRY_URL" | sed 's|^.*/\(.*\)$|\1|')
 
 echo "registry=$REGISTRY" >> $GITHUB_OUTPUT
+echo "repository=$REPOSITORY" >> $GITHUB_OUTPUT
